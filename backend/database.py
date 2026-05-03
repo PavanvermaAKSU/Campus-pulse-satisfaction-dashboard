1719eb6cb8b08caefd4d580e37458b0f8604f26a
Pavan Kumar Verma <vermapavan9078@gmail.com>
2026-05-03 13:42:23 +0530

    Replace DATABASE_URL with a placeholder
    Updated DATABASE_URL to a placeholder for security.

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:Pavan%409589@localhost/TCS_DB"
+DATABASE_URL = "__________________________________"
 
 engine = create_engine(DATABASE_URL)
 
